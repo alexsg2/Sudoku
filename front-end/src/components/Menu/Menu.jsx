afebcd7415c5fbbd5d91c6a0cc26c7a968ccdcc1
John Doe <johndoe@example.com>
2023-08-10 23:11:41 -0400

    Added Solve Button - Front End

--- front-end/src/components/Menu/Menu.jsx
+++ front-end/src/components/Menu/Menu.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 import "./Menu.css";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faPause, faCheck } from '@fortawesome/free-solid-svg-icons';
 
-const Menu = ({ difficulty, formattedTime, isPaused, onTogglePause }) => {
+const Menu = ({ difficulty, formattedTime, isPaused, onTogglePause, onSolve }) => {
   return (
     <div className="menu">
       <Link to="/" className="back-button">&lt; Back</Link>
@@ -15,6 +15,9 @@ const Menu = ({ difficulty, formattedTime, isPaused, onTogglePause }) => {
           {isPaused ? <FontAwesomeIcon icon={faPlay} /> : <FontAwesomeIcon icon={faPause} />}
         </button>
       </div>
+      <button onClick={onSolve} className="solve-button">
+        Solve <span className="solve-icon"><FontAwesomeIcon icon={faCheck} /></span>
+      </button>
       <p>{difficulty}</p>
     </div>
   );
