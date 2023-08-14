6f42c2c31e7d78209cb52408dc26b3de2ef97017
John Doe <johndoe@example.com>
2023-08-13 21:57:36 -0400

    Redesigned the Home Page

--- front-end/src/components/HomeMode/Home.jsx
+++ front-end/src/components/HomeMode/Home.jsx
@@ -9,17 +9,22 @@ const Home = () => {
       <h1 className="centered-title">Sudoku Game</h1>
       <hr />
 
+      <h2 className='welcome-header'>  Welcome to my Sudoku game! </h2>
+      <p className='welcome-message'> Prepare to challenge your mind and enjoy. Choose your preferred difficulty level before we start. </p>
+
       {/* Display a message to choose a puzzle difficulty */}
-      <p className="centered-message">Choose Your Puzzle Difficulty:</p>
+      <div className="rounded-rectangle-border">
+        <p className="centered-message">Choose Your Puzzle Difficulty:</p>
 
-      <div className="difficulty-buttons">
-        <div className="vertical-buttons">
+        <div className="difficulty-buttons">
+          <div className="vertical-buttons">
 
-          {/* Use 'Link' components to create buttons with routing */}
-          <Link to="/easy" className="rounded-button easy-button">Easy</Link>
-          <Link to="/medium" className="rounded-button medium-button">Medium</Link>
-          <Link to="/hard" className="rounded-button hard-button">Hard</Link>
-          
+            {/* Use 'Link' components to create buttons with routing */}
+            <Link to="/easy" className="rounded-button easy-button">Easy</Link>
+            <Link to="/medium" className="rounded-button medium-button">Medium</Link>
+            <Link to="/hard" className="rounded-button hard-button">Hard</Link>
+            
+          </div>
         </div>
       </div>
     </div>
